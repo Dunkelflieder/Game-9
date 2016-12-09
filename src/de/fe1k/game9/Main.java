5302f15afa5e07171c0ffaaacd5fcd1f04b1f35e
Nerogar <nerogar@arcor.de>
2016-12-09 17:32:25 +0100

    add System.err stream to warning and error messages

--- src/de/fe1k/game9/Main.java
+++ src/de/fe1k/game9/Main.java
@@ -4,7 +4,8 @@ import de.nerogar.noise.util.Logger;
 
 public class Main {
 	public static void main(String[] args) {
-		Logger.addStream(Logger.DEBUG, System.out);
+		Logger.addStream(Logger.DEBUG, Logger.INFO, System.out);
+		Logger.addStream(Logger.WARNING, System.err);
 
 		new Game().run();
 	}
