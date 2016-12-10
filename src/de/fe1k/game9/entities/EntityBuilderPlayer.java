182bca3a431ea0c146bde1b0466308120e18e0d7
Felk <studio.felk@googlemail.com>
2016-12-10 06:31:37 +0100

    tweak player bounding, so he doesn't float

--- src/de/fe1k/game9/entities/EntityBuilderPlayer.java
+++ src/de/fe1k/game9/entities/EntityBuilderPlayer.java
@@ -16,7 +16,7 @@ public class EntityBuilderPlayer implements Tile.EntityBuilder {
 		entity.getScale().set(1.0f);
 		entity.addComponent(new ComponentSpriteRenderer(Game.renderer, "man"));
 		entity.addComponent(new ComponentMoving());
-		entity.addComponent(new ComponentBounding(new Bounding(0.2f, 0, 0.8f, 0.95f), ComponentBounding.LAYER_PLAYER, ComponentBounding.LAYER_ALL));
+		entity.addComponent(new ComponentBounding(new Bounding(0.2f, 0.1f, 0.9f, 0.95f), ComponentBounding.LAYER_PLAYER, ComponentBounding.LAYER_ALL));
 		entity.addComponent(new ComponentLight(Game.renderer, new Color(1.0f, 0.8f, 0.8f, 0.0f), 20, 3));
 		entity.addComponent(new ComponentDeathAnimation());
 		ComponentControllable control = new ComponentControllable(Game.window.getInputHandler());
