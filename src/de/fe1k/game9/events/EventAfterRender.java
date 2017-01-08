cb4a4bdf3ddeea5ef32046b540038d63b32b3a95
Felk <studio.felk@googlemail.com>
2017-01-08 21:41:50 +0100

    fix EventAfterRender inheriting from EventBeforeRender when it shouldn't

--- src/de/fe1k/game9/events/EventAfterRender.java
+++ src/de/fe1k/game9/events/EventAfterRender.java
@@ -2,8 +2,13 @@ package de.fe1k.game9.events;
 
 import de.nerogar.noise.render.GLWindow;
 
-public class EventAfterRender extends EventBeforeRender {
+public class EventAfterRender implements Event {
+	public GLWindow window;
+	public float deltaTime;
+	public double runTime;
 	public EventAfterRender(GLWindow window, float deltaTime, double runTime) {
-		super(window, deltaTime, runTime);
+		this.window = window;
+		this.deltaTime = deltaTime;
+		this.runTime = runTime;
 	}
 }
